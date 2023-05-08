2e72087ba02a22d3d122d589cdb187d19902fbd1
juan-coralogix <89215136+juan-coralogix@users.noreply.github.com>
2023-05-08 12:21:08 -0300

    cleanup & fix condition. (#5)

--- modules/logpush-job/main.tf
+++ modules/logpush-job/main.tf
@@ -64,25 +64,6 @@ resource "random_string" "this" {
   upper = false
 }
 
-
-#resource "cloudflare_logpush_job" "crx-logpush-zone" {
-#  count = local.dataset_type[var.cloudflare_logpush_dataset] == "zone" ? 1 : 0 
-#  enabled             = true
-#  zone_id = var.cloudflare_zone_id
-#  name                = local.job_name
-#  logpull_options     = "fields=${coalesce(var.cloudflare_logpush_fields,local.dataset_full_fields[var.cloudflare_logpush_dataset])},${local.dataset_timestamp[var.cloudflare_logpush_dataset]}&timestamps=unixnano"
-#  destination_conf = "https://${local.coralogix_regions[var.coralogix_region]}/api/v1/cloudflare/logs?header_Authorization=Bearer%20${var.coralogix_private_key}&header_CX-Application-Name=${var.coralogix_application_name}&header_CX-Subsystem-Name=${var.coralogix_subsystem_name}&header_timestamp-format=UnixNano&header_dataset=${local.coralogix_dataset[var.cloudflare_logpush_dataset]}&tags=dataset:${var.cloudflare_logpush_dataset}"
-#  dataset             = var.cloudflare_logpush_dataset
-#  frequency = "low"
-#  lifecycle {
-#    #precondition {
-#    #  condition     = length(var.cloudflare_zone_id) > 0
-#    #  error_message = "To create logpush with a zone-scoped dataset, zone-id must be set."
-#    #}
-#  }
-#}
-
-
 resource "cloudflare_logpush_job" "crx-logpush-zone" {
   count = local.dataset_type[var.cloudflare_logpush_dataset] == "zone" ? 1 : 0
   enabled             = true
@@ -105,7 +86,7 @@ resource "cloudflare_logpush_job" "crx-logpush-account" {
   account_id = var.cloudflare_account_id
   name                = local.job_name
   logpull_options     = "fields=${coalesce(var.cloudflare_logpush_fields,local.dataset_full_fields[var.cloudflare_logpush_dataset])},${local.dataset_timestamp[var.cloudflare_logpush_dataset]}&timestamps=unixnano"
-  destination_conf = var.coralogix_subsystem_name != "" || var.coralogix_application_name != "" ? "https://${local.coralogix_regions[var.coralogix_region]}/api/v1/cloudflare/logs?header_Authorization=Bearer%20${var.coralogix_private_key}&header_CX-Application-Name=${var.coralogix_application_name}&header_CX-Subsystem-Name=${var.coralogix_subsystem_name}&header_timestamp-format=UnixNano&header_dataset=${local.coralogix_dataset[var.cloudflare_logpush_dataset]}&tags=dataset:${var.cloudflare_logpush_dataset}"
+  destination_conf = var.coralogix_subsystem_name != "" || var.coralogix_application_name != "" ? "https://${local.coralogix_regions[var.coralogix_region]}/api/v1/cloudflare/logs?header_Authorization=Bearer%20${var.coralogix_private_key}&header_CX-Application-Name=${var.coralogix_application_name}&header_CX-Subsystem-Name=${var.coralogix_subsystem_name}&header_timestamp-format=UnixNano&header_dataset=${local.coralogix_dataset[var.cloudflare_logpush_dataset]}&tags=dataset:${var.cloudflare_logpush_dataset}" : "https://${local.coralogix_regions[var.coralogix_region]}/api/v1/cloudflare/logs?header_Authorization=Bearer%20${var.coralogix_private_key}&header_timestamp-format=UnixNano&header_dataset=${local.coralogix_dataset[var.cloudflare_logpush_dataset]}&tags=dataset:${var.cloudflare_logpush_dataset}"
   dataset             = var.cloudflare_logpush_dataset
   frequency = "low"
   filter = ""
